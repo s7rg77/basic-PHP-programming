50a20c92e90a364d45ecac8ef385f3009647bdcf
s7rg77 <sxrgxx@gmail.com>
2024-07-31 11:57:53 +0200

    01

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>desarrollo web entorno servidor</title>
+    <title>basic PHP programming</title>
     <style>
         body {
             margin: 0px;
@@ -54,7 +54,7 @@
         button {
             margin-left: 10px;
             padding: 5px;
-            border: 1px solid #006400;
+            border: none;
             background-color: #006400;
             color: #00FF00;
             cursor: pointer;
@@ -65,12 +65,12 @@
         }
 
         footer {
+            bottom: 0px;
+            width: 100%;
             background-color: #006400;
             color: #00FF00;
             text-align: center;
             position: fixed;
-            bottom: 0;
-            width: 100%;
         }
     </style>
     <script>
